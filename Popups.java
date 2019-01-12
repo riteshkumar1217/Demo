05915e6a6caed77b4b44b8c8238fdc2c5709be89
riteshkumar1217 <ritesh.kumar@hirepro.in>
2019-01-12 11:25:25 +0530

    New file popup included

---
+++ Popups.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Popups {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "D:/chromedriver.exe");
+		WebDriver driver =new ChromeDriver();
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		driver.get("http://toolsqa.com/handling-alerts-using-selenium-webdriver/");
+		String a = driver.getWindowHandle();
+		driver.findElement(By.xpath("//*[@id="content"]/p[4]/button")).click();
+		driver.switchTo().alert().accept();
+
+	}
+
+}
